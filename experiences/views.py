236c7f921f07eec9d67ccedfada695f7e966f3db
jinwook song <wlsdnr129@naver.com>
2022-10-15 23:40:44 +0900

    feat: perks and perk detail

--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from experiences.models import Perk
+from experiences.serializers import PerkSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 
-# Create your views here.
+
+class Perks(APIView):
+    def get(self, req):
+        all_perks = Perk.objects.all()
+        serializer = PerkSerializer(all_perks, many=True)
+        return Response(serializer.data)
+
+    def post(self, req):
+        serializer = PerkSerializer(data=req.data)
+        if serializer.is_valid():
+            new_amenity = serializer.save()
+            return Response(PerkSerializer(new_amenity).data)
+        else:
+            return Response(serializer.errors)
+
+
+class PerkDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Perk.objects.get(pk=pk)
+        except Perk.DoesNotExist:
+            raise NotFound
+
+    def get(self, req, pk):
+        amenity = self.get_object(pk=pk)
+        serializer = PerkSerializer(amenity)
+        return Response(serializer.data)
+
+    def put(self, req, pk):
+        amenity = self.get_object(pk=pk)
+        serializer = PerkSerializer(amenity, data=req.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, req, pk):
+        amenity = self.get_object(pk=pk)
+        amenity.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
--- rooms/urls.py
+++ rooms/urls.py
@@ -3,6 +3,6 @@ from rooms import views
 
 
 urlpatterns = [
-    path("amenities", views.Amenities.as_view()),
+    path("amenities/", views.Amenities.as_view()),
     path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
