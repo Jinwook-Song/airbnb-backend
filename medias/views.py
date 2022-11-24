31d2b1fec99a0c23756f081af3f670cbe2ddcb61
jinwook song <wlsdnr129@naver.com>
2022-11-24 21:39:48 +0900

    feat: get one time upload url from CF

--- medias/views.py
+++ medias/views.py
@@ -1,9 +1,29 @@
+from django.conf import settings
+import requests
 from rest_framework.views import APIView
-from medias.models import Photo
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.permissions import IsAuthenticated
+from medias.models import Photo
+
+
+class GetUploadURL(APIView):
+    def post(self, req):
+        base_url = "https://api.cloudflare.com/client/"
+        url = f"{base_url}v4/accounts/{settings.CF_ACCOUNT_ID}/images/v2/direct_upload"
+        one_time_url = requests.post(
+            url,
+            headers={
+                "Authorization": f"Bearer {settings.CF_TOKEN}",
+            },
+        )
+        one_time_url = one_time_url.json()
+        result = one_time_url.get("result")
+        uploadURL = result.get("uploadURL")
+        id = result.get("id")
+
+        return Response({"uploadURL": uploadURL, "id": id})
 
 
 class PhotoDetail(APIView):
